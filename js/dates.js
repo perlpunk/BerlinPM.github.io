fda8ce5075083fb178af0c6dbe649a1d949b8b2f
Tina Mueller <tinita@cpan.org>
2014-05-29 21:07:49 +0200

    stylesheet, perl foundation logo, list next dates

---
+++ js/dates.js
@@ -0,0 +1,13 @@
+var url = "http://perlpunks.de/dates.json";
+url = "js/dates.json";
+$.getJSON( url, function( data ) {
+  var items = [];
+  $.each( data, function( index, value ) {
+    items.push( "<li id='" + index + "'>" + value.date + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "date-list",
+    html: items.join( "" )
+  }).appendTo( "#dates" );
+});
